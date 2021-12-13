a9c85dccb26dee0c8100ff245ab2e70672dc1aa3
tatyana <fcognitive@gmail.com>
2021-12-13 11:35:10 +0300

    Refactoring Details For CurrentWeatherView in ScrollView and change padding

--- WeatherProjectTestOrion/View/DetailsForCurrentWeatherView.swift
+++ WeatherProjectTestOrion/View/DetailsForCurrentWeatherView.swift
@@ -13,9 +13,65 @@ struct DetailsForCurrentWeatherView: View {
             DescriptionWithIconView()
             HourlyCurrentWeatherView()
         }
+        .frame(height: 88)
+//        .border(.red, width: 2)
+    }
+}
+
+
+struct HourlyCurrentWeatherView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(hourlyCurrentWeather, id: \.self) { item in
+                    VStack {
+                        Text(item.hour)
+                            .font(.system(size: 12, weight: .regular))
+                                                            .padding(.top, 8)
+                        
+                            .border(.black, width: 1)
+                        
+                        Image(systemName: item.icon)
+                            .offset(y:-2)
+                            .frame(width: 24, height: 24)
+                            .border(.red, width: 2)
+                        Text(item.temp)
+                            .font(.system(size: 12, weight: .regular))
+                            .padding(.bottom, 8)
+                        
+                    }
+                    .border(.green, width: 2)
+                    //                            .padding(.top, 8)
+                    //                            .padding(.bottom, 8)
+                }
+            }
+            
+            
+        }
+        .frame(height: 88)
+        .border(.yellow, width: 2)
+    }
+}
+
+
+
+
+struct DescriptionWithIconView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            WindIconWithDescriptionView()
+            PressureIconWithDescriptionView()
+            HumidityIconWithDescriptionView()
+        }
+        .border(.yellow, width: 2)
+        //        .padding(.top, 0)
+        .font(.system(size: 12, weight: .regular))
         
     }
 }
+
+
+
 
 struct DetailsForCurrentWeatherView_Previews: PreviewProvider {
     static var previews: some View {
@@ -28,79 +84,51 @@ struct DetailsForCurrentWeatherView_Previews: PreviewProvider {
     }
 }
 
-struct HourlyCurrentWeatherView: View {
+struct WindIconWithDescriptionView: View {
     var body: some View {
-            ScrollView(.horizontal) {
-                    HStack {
-                        ForEach(hourlyCurrentWeather, id: \.self) { item in
-                            VStack {
-                                VStack {
-                                    Text(item.hour)
-                                    .font(.system(size: 12, weight: .regular))
-                                }
-                                VStack {
-                                    Image(systemName: item.icon)
-                                        
-                                        .frame(width: 24, height: 24)
-                                }
-                                VStack {
-                                    Text(item.temp)
-                                    .font(.system(size: 12, weight: .regular))
-                                }
-                                
-                                    
-                            }
-                            .padding(.top, 8)
-                            .padding(.bottom, 8)
-                        }
-                    }
-                    
-                    
-                }
-
+        VStack {
+            HStack(spacing: 9.5) {
+                Image(systemName: "wind")
+                    .padding(.leading, 18.25)
+                    .padding(.top, 8.5)
+                    .padding(.bottom, 13)
+                    .border(.red, width: 2)
+                Text("10 km/h, SE")
+                    .padding(.top, 8)
+                    .padding(.bottom, 12)
+                    .border(.red, width: 2)
+            }
+            
+        }
     }
 }
 
-
-
-
-struct DescriptionWithIconView: View {
+struct PressureIconWithDescriptionView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            VStack {
-                HStack(spacing: 9.5) {
-                        Image(systemName: "wind")
-                            .padding(.leading, 18.25)
-                            .padding(.top, 8.5)
-                            .padding(.bottom, 13)
-                        Text("10 km/h, SE")
-                            .padding(.top, 8)
-                            .padding(.bottom, 12)
-                }
+        VStack {
+            HStack(spacing: 9.5) {
+                Image(systemName: "safari")
+                    .padding(.leading, 17.5)
+                    .padding(.bottom, 14.33)
+                Text("983 hPa")
+                    .padding(.bottom, 12)
             }
-            VStack {
-                HStack(spacing: 9.5) {
-                        Image(systemName: "safari")
-                            .padding(.leading, 17.5)
-                            .padding(.bottom, 14.33)
-                        Text("983 hPa")
-                        .padding(.bottom, 12)
-                }
+            .border(.green, width: 2)
+        }
+    }
+}
+
+struct HumidityIconWithDescriptionView: View {
+    var body: some View {
+        VStack {
+            HStack(spacing: 12.5) {
+                Image(systemName: "drop")
+                    .padding(.leading, 20.25)
+                    .padding(.bottom, 9.83)
+                Text("83%")
+                    .padding(.bottom, 8)
             }
             
-            VStack {
-                HStack(spacing: 12.5) {
-                        Image(systemName: "drop")
-                            .padding(.leading, 20.25)
-                            .padding(.bottom, 9.83)
-                        Text("83%")
-                            .padding(.bottom, 8)
-                    }
-                
-            }
         }
-        .padding(.top, 0)
-        .font(.system(size: 12, weight: .regular))
-        
     }
 }
