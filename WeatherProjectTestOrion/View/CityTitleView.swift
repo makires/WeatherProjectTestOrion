bd96115d3138dbb888e336eed98dc84654401feb
tatyana <fcognitive@gmail.com>
2021-12-14 15:52:00 +0300

    GeometryReader broke MainView into ZStack, width CityTitleVIew change to infinity

--- WeatherProjectTestOrion/View/CityTitleView.swift
+++ WeatherProjectTestOrion/View/CityTitleView.swift
@@ -9,40 +9,44 @@ import SwiftUI
 
 struct CityTitleView: View {
     var body: some View {
-        GeometryReader { gr in
-            HStack(alignment: .bottom) {
-                VStack(alignment: .leading) {
-                    Text("Nizhny Novgorod")
-                        
-                        .font(.largeTitle)
-                        .lineSpacing(41)
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                }
-                .border(.red, width: 2)
-                .padding(.leading, 16)
-                .padding(.top, 39)
-                .padding(.bottom, 8)
-                Spacer()
-                VStack {
-                    Button(action: {
-                        print("")
-                    }, label: {
-                        Image(systemName: "list.bullet")
-                            .font(.body)
+        HStack {
+//            GeometryReader { gr in
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Nizhny Novgorod")
+                            
+                            .font(.largeTitle)
+                            .lineSpacing(41)
                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                    })
+                    }
+                    .border(.red, width: 2)
+                    .padding(.leading, 16)
+                    .padding(.top, 39)
+                    .padding(.bottom, 8)
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            print("")
+                        }, label: {
+                            Image(systemName: "list.bullet")
+                                .font(.body)
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        })
+                        
+                            .frame(width: 44, height: 44)
+                    }
+                    .border(.red, width: 2)
+                    .padding(.top, 44)
+                    .padding(.trailing, 10)
+                    .padding(.bottom, 0)
                     
-                        .frame(width: 44, height: 44)
                 }
                 .border(.red, width: 2)
-                .padding(.top, 44)
-                .padding(.trailing, 10)
-                .padding(.bottom, 0)
                 
-            }
-            .border(.red, width: 2)
-//            .edgesIgnoringSafeArea(.horizontal)
-            .frame(width: gr.size.width, height: 88)
+//                .frame(width: gr.size.width, height: 88)
+                .frame(width: .infinity, height: 88)
+//                .edgesIgnoringSafeArea(.horizontal)
+//            }
         }
         
         
