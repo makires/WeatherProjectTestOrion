bd3e2d518591d0853f477a4c457c254f5271882d
tatyana <fcognitive@gmail.com>
2021-12-14 15:27:35 +0300

    Change frames and sizes for CityTitleView throughout GeometryReader

--- WeatherProjectTestOrion/View/CityTitleView.swift
+++ WeatherProjectTestOrion/View/CityTitleView.swift
@@ -9,34 +9,42 @@ import SwiftUI
 
 struct CityTitleView: View {
     var body: some View {
-        HStack(alignment: .bottom) {
-            VStack(alignment: .leading) {
-                Text("Nizhny Novgorod")
-                    .font(.largeTitle)
-                    .lineSpacing(41)
-                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-            }
-            .padding(.leading, 16)
-            .padding(.top, 39)
-            .padding(.bottom, 8)
-            Spacer()
-            VStack {
-                Button(action: {
-                    print("")
-                }, label: {
-                    Image(systemName: "list.bullet")
-                        .font(.body)
+        GeometryReader { gr in
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading) {
+                    Text("Nizhny Novgorod")
+                        
+                        .font(.largeTitle)
+                        .lineSpacing(41)
                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                })
+                }
+                .border(.red, width: 2)
+                .padding(.leading, 16)
+                .padding(.top, 39)
+                .padding(.bottom, 8)
+                Spacer()
+                VStack {
+                    Button(action: {
+                        print("")
+                    }, label: {
+                        Image(systemName: "list.bullet")
+                            .font(.body)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    })
+                    
+                        .frame(width: 44, height: 44)
+                }
+                .border(.red, width: 2)
+                .padding(.top, 44)
+                .padding(.trailing, 10)
+                .padding(.bottom, 0)
                 
-                    .frame(width: 44, height: 44)
             }
-            .padding(.top, 44)
-            .padding(.trailing, 10)
-            .padding(.bottom, 0)
-            
+            .border(.red, width: 2)
+//            .edgesIgnoringSafeArea(.horizontal)
+            .frame(width: gr.size.width, height: 88)
         }
-        .frame(width: 375, height: 88)
+        
         
     }
 }
