841a43f80828d2c527d42ecf4e4b1826dfabf6e2
tatyana <fcognitive@gmail.com>
2021-12-29 01:11:19 +0300

    Separate file for HourlyCurrentWeather * it is model for weather with use in views

--- WeatherProjectTestOrion/Main/Model/Weather.swift
+++ WeatherProjectTestOrion/Main/Model/Weather.swift
@@ -32,30 +32,7 @@ extension Weather {
     }
 }
 
-struct HourlyCurrentWeather {
-    var hours: [Hour] = []
-    func createCurrentHours(response: APIForecastWeatherModel) -> [Hour] {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-        let currentDate = dateFormatter.date(from: response.location.currentLocalTime) ?? Date()
-        var arrayHoursAll: [Hour] = []
-        var arrayHoursPublic: [Hour] = []
-        arrayHoursAll = response.forecast.forecastday[0].hour
-        for hour in arrayHoursAll {
-            if Date(timeIntervalSince1970: TimeInterval(hour.timeEpoch)) < currentDate {
-                // nothing do, next iteration
-            } else if Date(timeIntervalSince1970: TimeInterval(hour.timeEpoch)) >= currentDate {
-                arrayHoursPublic.append(hour)
-            }
-        }
-        return arrayHoursPublic
-    }
-}
-extension HourlyCurrentWeather {
-    init(response: APIForecastWeatherModel) {
-        hours = createCurrentHours(response: response)
-    }
-}
+
 
 struct DailyForecats {
     var days: [Forecastday] = []
