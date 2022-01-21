4691fcc159c0565acee654446955fea170f397b1
tatyana <id.tash@gmail.com>
2022-01-21 12:34:44 +0300

    Add Arrays Extensions.

---
+++ WeatherProjectTestOrion/Extensions+/Array+.swift
@@ -0,0 +1,19 @@
+//
+//  Array+.swift
+//  WeatherProjectTestOrion
+//
+//  Created by Tatyana Ilvutikova on 21.01.2022.
+//
+
+import Foundation
+
+extension Array where Element == String {
+  func encodeArray() -> Data? {
+    let encoder = JSONEncoder()
+    if let encoded = try? encoder.encode(self) {
+      return encoded
+    } else {
+      return nil
+    }
+  }
+}
