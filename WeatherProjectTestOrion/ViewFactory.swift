4a880c189d67a8eda840103127a1b21958804b04
Ultaris <47312326+makires@users.noreply.github.com>
2022-01-25 09:49:14 +0300

    Types for ViewFactory

--- WeatherProjectTestOrion/ViewFactory.swift
+++ WeatherProjectTestOrion/ViewFactory.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct ViewFactory {
-
+  enum TypeView {
+    case main
+    case list
+    case search
+  }
+  func makeView(typeView: TypeView) {
+    switch typeView {
+    case .main:
+      let mainView = MainView()
+    case .list:
+      let citiesListView = ListLocationsView()
+    case .search:
+      let searchCities = SearchCitiesView()
+    }
+  }
 }
-let mainView = MainView()
-let citiesListView = ListLocationsView()
-let searchCities = SearchCitiesView()
+
+
+
 
 
 
